4a754e9ff0db92812c07b173a203651384c94e61
LuBuCake <madneshell@outlook.com>
2021-03-27 19:19:55 -0300

    Initial Commit

---
+++ GameX/App.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Windows.Forms;
+using GameX.Modules;
+using GameX.Helpers;
+using System.Diagnostics;
+
+namespace GameX
+{
+    public partial class App : DevExpress.XtraEditors.XtraForm
+    {
+        /*App Properties*/
+
+        private bool Initialized { get; set; }
+        private KernelAccess Kernel { get; set; }
+        private Process pProcess { get; set; }
+        private MessagePeaker Peaker { get; set; }
+
+        private string TargetProcess = "";
+        private string TargetVersion = "";
+        private string[] TargetModules = { };
+
+        /*App Init*/
+
+        public App()
+        {
+            InitializeComponent();
+
+            Peaker = new MessagePeaker();
+
+            Application.Idle += Application_Idle;
+            Application.ApplicationExit += Application_ApplicationExit;
+        }
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            while (Peaker.IsApplicationIdle())
+            {
+                Think();
+            }
+        }
+
+        private void Application_ApplicationExit(object sender, EventArgs e)
+        {
+            Application.Idle += null;
+        }
+
+        /*App Methods*/
+
+        private bool HandleProcess()
+        {
+            if (pProcess == null)
+            {
+                pProcess = KernelHelper.GetProcessByName(TargetProcess);
+                Initialized = false;
+            }
+            else
+            {
+                if (!Initialized)
+                {
+                    if (ValidateTarget(pProcess))
+                    {
+                        pProcess.EnableRaisingEvents = true;
+                        pProcess.Exited += ClearAndRestore;
+                        Kernel = new KernelAccess(pProcess);
+                        Initialized = true;
+                    }
+                }
+            }
+
+            return Initialized;
+        }
+
+        private bool ValidateTarget(Process pProcess)
+        {
+            if (TargetVersion != "" && !pProcess.MainModule.FileVersionInfo.ToString().Contains(TargetVersion))
+                return false;
+
+            if (TargetModules.Length > 0)
+            {
+                foreach (string Module in TargetModules)
+                {
+                    if (!KernelHelper.ProcessHasModule(pProcess, Module))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void ClearAndRestore(object sender, EventArgs e)
+        {
+            pProcess.Dispose();
+            pProcess = null;
+            Kernel.Dispose();
+            Kernel = null;
+        }
+
+        private void Think()
+        {
+            if (HandleProcess())
+            {
+
+            }
+        }
+
+    }
+}
---
+++ GameX/Helpers/MessagePeaker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameX.Helpers
+{
+    public class MessagePeaker
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct NativeMessage
+        {
+            public IntPtr Handle;
+            public uint Message;
+            public IntPtr WParameter;
+            public IntPtr LParameter;
+            public uint Time;
+            public Point Location;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int PeekMessage(out NativeMessage message, IntPtr window, uint filterMin, uint filterMax, uint remove);
+
+        public bool IsApplicationIdle()
+        {
+            NativeMessage result;
+            return PeekMessage(out result, IntPtr.Zero, 0, 0, 0) == 0;
+        }
+    }
+}
